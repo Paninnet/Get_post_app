b003b18043c490f6b25083301c4994ff8cb9a608
PaninNet <iliapanin300999@mail.ru>
2021-08-25 15:58:00 +0300

    delete all. Create new

--- src/App.js
+++ src/App.js
@@ -1,49 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { useRef, useState } from 'react';
-import { PostList } from './Components/postlist/PostList';
-import { PostForm } from './Components/PostForm';
-import { MySelect } from './Components/UI/select/MySelect';
+
 
 function App() {
-  const [post, setPost] = useState([
-    { id: 1, title: "HTML", body: "Learn HTML" },
-    { id: 2, title: "CSS", body: "Learn CSS" },
-    { id: 3, title: "JS", body: "Learn JS" },
-    { id: 4, title: "REACT.JS", body: "Learn REACT.JS" },
-  ])
-
-  const createPost = (newPost) => {
-    setPost([newPost, ...post])
-
-  }
-
-  const removePost = (postId) => {
-    setPost(post.filter(item => item.id !== postId))
-  }
-
-  const [select, setSelect] = useState('')
-
-  const selectedSelect = (newSelect) =>{
-    setSelect(newSelect)
-    setPost([...post].sort((a,d) => a[newSelect].localeCompare(d[newSelect])))
-    console.log(newSelect);
-  }
 
 
   return (
     <div className="App">
-      <PostForm create={createPost} />
-      <MySelect defaultSelect="Selected by" 
-      options={[
-        {value:'title', text:'Title'},
-        {value:'body', text:'Description'}
-      ]}
-      selectedSelect={selectedSelect}
-      value={select}
-      />
-      {post.length !== 0 ? <PostList deletePost={removePost} posts={post} /> : <h1>Post not Found</h1>}
-
+     
     </div>
   );
 }
